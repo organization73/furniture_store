8bf681d5aa478e94e683ed46ac3d8b7569204323
Elsharabasy73 <abdo.make73@gmail.com>
2024-03-04 13:46:13 +0200

    [Add] setup a signin websocket listener in the api

--- server/socketio/socketHelper.js
+++ server/socketio/socketHelper.js
@@ -1,22 +1,31 @@
 const io = require("./socket").getIO();
 
 module.exports.signin = (socket, onlineUsers) => {
+  let userType = "user";
   socket.on("signin", (token) => {
-    console.log("signin token:", token);
+    if (!token) {
+      //check if token is in the cookie (admin)
+      const token = socket.handshake.headers.cookie.split("=")[1];
+      userType = "admin";
+      console.log("token:", token);
+    }
     if (!token) {
       return socket.emit("authError", { message: "No token provided" });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decoded.userId;
     try {
-      const user = User.findById(userId);
-      if (!user) {
+      let user;
+      if (userType === "admin") {
         user = Admin.findById(userId);
+      } else {
+        user = User.findById(userId);
       }
       if (!user) {
-        return socket.emit("authError", { message: "Invalid token" });
+        return socket.emit("authError", { message: "Invalid token}" });
       }
-      onlineUsers[userId] = socket.id;
+      let userObj = { userID: user._id, userType, socketId: socket.id };
+      onlineUsers.push(userObj);
       console.log("onlineUsers:", onlineUsers);
       socket.emit("authSuccess", { message: "Auth success" });
     } catch (error) {
