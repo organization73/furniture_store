53e192cf0f74b0be53e7cca1f658ccd43b0f6138
Elsharabasy73 <abdo.make73@gmail.com>
2024-03-29 22:57:09 +0200

    [Add] pagination functionality to admin products page

--- server/adminController/shop.js
+++ server/adminController/shop.js
@@ -24,15 +24,29 @@ module.exports.getChat = (req, res, next) => {
 module.exports.getProducts = async (req, res, next) => {
   const page = req.query.page || 1;
   try {
+    const totalItems = await Product.find().countDocuments();
     const products = await Product.find()
       .skip(PRODUCTS_PER_PAGE * (page - 1))
       .limit(PRODUCTS_PER_PAGE);
+
+      console.log("currentPage", page);
+      console.log("hasNextPage", PRODUCTS_PER_PAGE * page < totalItems);
+      console.log("hasPreviousPage", page > 1);
+      console.log("nextPage", page + 1);
+      console.log("previousPage", page - 1);
+      console.log("lastPage", Math.ceil(totalItems / PRODUCTS_PER_PAGE));
     res.render("admin/products", {
       path: "/products",
       pageTitle: "Products",
       errorMessage: null, // Pass the stored flash message to the view
       isAuthenticated: req.admin ? true : false,
       products: products,
+      currentPage: +page,
+      hasNextPage: PRODUCTS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: +page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / PRODUCTS_PER_PAGE)
     });
   } catch (err) {
     next(err);
