1740367bb5d7ef83c1e23aabe8a3957dd03c866f
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-05-04 18:23:27 +0300

    [remove] unwanted snack bars

--- lib/features/home/screens/search/controllers/recent_search_controller.dart
+++ lib/features/home/screens/search/controllers/recent_search_controller.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-
-import 'package:decordash/common/widgets/loaders/loaders.dart';
 import 'package:decordash/utils/local_storage/storage_utility.dart';
 import 'package:get/get.dart';
 
@@ -24,36 +22,22 @@ class RecentSearchController extends GetxController {
   }
 
   void addSearch(String query) {
-  if (!recentSearches.contains(query)) {
-    // Check if the list already contains 5 items
-    if (recentSearches.length >= 5) {
-      // If it does, remove the oldest item before adding the new one
-      recentSearches.removeAt(0);
+    if (!recentSearches.contains(query)) {
+      // Check if the list already contains 5 items
+      if (recentSearches.length >= 5) {
+        // If it does, remove the oldest item before adding the new one
+        recentSearches.removeAt(0);
+      }
+      // Add the new search query to the list
+      recentSearches.add(query);
+      saveRecentSearchesToStorage();
     }
-    // Add the new search query to the list
-    recentSearches.add(query);
-    saveRecentSearchesToStorage();
-    TLoaders.customToast(
-        messege: 'Search query has been added to recent searches');
-  } else {
-    TLoaders.customToast(
-        messege: 'Search query is a duplicate. Cannot add duplicates.');
   }
-}
-
 
   void removeSearch(int query) {
     recentSearches.removeAt(query);
     saveRecentSearchesToStorage();
     recentSearches.refresh();
-    TLoaders.customToast(
-        messege: 'Search query has been removed from recent searches');
-  }
-
-  void displayRecentSearches() {
-    // Assuming you have a method to display the list of recent searches
-    // This could be a simple print statement or a more complex UI update
-    print(recentSearches);
   }
 
   void saveRecentSearchesToStorage() {
