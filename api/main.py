93be5dba7b85abb49dab30222d57ef31520fe2d7
Elsharabasy73 <abdo.make73@gmail.com>
2024-05-12 23:03:15 +0300

    [Add] model accept imageUrl

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 import uvicorn
 from PIL import Image
 import tensorflow as tf
 from io import BytesIO
 import numpy as np
 import os
+import requests
+
 
 app = FastAPI()
 
@@ -27,21 +29,57 @@ def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
-@app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
-):
-    image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
+# @app.post("/predict")
+# async def predict(
+#     file: UploadFile = File(...)
+# ):
+#     image = read_file_as_image(await file.read())
+#     img_batch = np.expand_dims(image, 0)
     
-    predictions = model.predict(img_batch)
+#     predictions = model.predict(img_batch)
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
-    return {
-        'class': predicted_class,
-        'confidence': float(confidence)
-    }
+#     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+#     confidence = np.max(predictions[0])
+#     return {
+#         'class': predicted_class,
+#         'confidence': float(confidence)
+#     }
+
+@app.post("/predict")
+async def predict(image_url: str):
+    print("image_url: ", image_url)
+    try:
+        # Download the image
+        response = requests.get(image_url)
+        response.raise_for_status()
+        image_data = response.content
+
+        # Save the image temporarily
+        image_path = os.path.join("images", "temp_image.jpg")
+        with open(image_path, "wb") as f:
+            f.write(image_data)
+
+        # Read the saved image
+        image = read_file_as_image(image_data)
+
+        # Preprocess the image for prediction
+        img_batch = np.expand_dims(image, 0)
+
+        # Make predictions
+        predictions = model.predict(img_batch)
+        predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+
+        # Clean up the temporary image file
+        os.remove(image_path)
+
+        return {'class': predicted_class, 'confidence': float(confidence)}
+
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=400, detail=f"Error downloading image: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during prediction: {e}")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
