0725e5507bf058fc483042d67d818a40214beef1
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-05-15 13:10:19 +0300

    delete all files

--- android/app/src/main/kotlin/com/decordash/decordash/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.decordash.decordash
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/constants/api_constants.dart
+++
@@ -1,3 +0,0 @@
-/* -- LIST OF Constants used in APIs -- */
-
-const String aiAPIKey = "sk-fwETRaDrr7XLNAnNXRtd4bKTv0KayLTyNnqJqP3gqRPHQzzz";
