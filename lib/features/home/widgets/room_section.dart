87f54ddd263f6a5e7ac6f7ef46ac7b197ea7e4b9
shalabycr7 <abdoshalaby.dev@gmail.com>
2024-06-14 20:04:10 +0300

    [update] design

--- lib/features/home/widgets/room_section.dart
+++ lib/features/home/widgets/room_section.dart
@@ -38,25 +38,25 @@ class BuildRoomsSection extends StatelessWidget {
             return Center(child: Text('noCategories'.tr));
           } else {
             return SizedBox(
-              height: 160.h,
-              child: ListView.separated(
+              height: 150.h,
+              child: ListView.builder(
                 shrinkWrap: true,
-                separatorBuilder: (_, __) => SizedBox(
-                  width: TSizes.spaceBtwItems,
-                ),
                 scrollDirection: Axis.horizontal,
                 itemCount: controller.roomsCatedories.length,
                 itemBuilder: (context, index) {
                   final category = controller.roomsCatedories[index];
-                  return VerticalCategory(
-                      onTap: () => Get.to(
-                            () => SubCategoryScreen(
-                              category: category,
+                  return Padding(
+                    padding: EdgeInsets.only(right: TSizes.spaceBtwItems),
+                    child: VerticalCategory(
+                        onTap: () => Get.to(
+                              () => SubCategoryScreen(
+                                category: category,
+                              ),
+                              duration: const Duration(milliseconds: 300),
+                              transition: Transition.rightToLeft,
                             ),
-                            duration: const Duration(milliseconds: 300),
-                            transition: Transition.rightToLeft,
-                          ),
-                      category: category);
+                        category: category),
+                  );
                 },
               ),
             );
@@ -66,57 +66,3 @@ class BuildRoomsSection extends StatelessWidget {
     );
   }
 }
-
-  //  SingleChildScrollView(
-  //         padding: const EdgeInsets.symmetric(
-  //           vertical: 10,
-  //         ),
-  //         scrollDirection: Axis.horizontal,
-  //         child: Row(
-  //           children: listIteams.entries.map((entry) {
-  //             var sectionName = entry.key;
-  //             var image = entry.value;
-
-  //             return InkWell(
-  //               onTap: () => Get.to(
-  //                 () => CategoriesPage(
-  //                     categoryName: lang == 'ar'
-  //                         ? 'غرف $sectionName'
-  //                         : '$sectionName Room'),
-  //                 duration: const Duration(milliseconds: 300),
-  //                 transition: Transition.rightToLeft,
-  //               ),
-  //               child: Container(
-  //                 width: 130,
-  //                 height: 195,
-  //                 decoration: BoxDecoration(
-  //                   borderRadius: BorderRadius.circular(8.0),
-  //                   image: DecorationImage(
-  //                     image: AssetImage(image),
-  //                     fit: BoxFit.cover,
-  //                   ),
-  //                 ),
-  //                 padding:
-  //                     const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15),
-  //                 margin: const EdgeInsets.symmetric(horizontal: 8),
-  //                 child: Row(
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: [
-  //                     lang == 'ar'
-  //                         ? Text('غرف\n$sectionName',
-  //                             style: Theme.of(context)
-  //                                 .textTheme
-  //                                 .bodyMedium
-  //                                 ?.copyWith(color: Colors.black))
-  //                         : Text('$sectionName\nRoom',
-  //                             style: Theme.of(context)
-  //                                 .textTheme
-  //                                 .bodyMedium
-  //                                 ?.copyWith(color: Colors.black)),
-  //                   ],
-  //                 ),
-  //               ),
-  //             );
-  //           }).toList(),
-  //         ),
-  //       )
